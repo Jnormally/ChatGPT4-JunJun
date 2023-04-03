6d1fa92cb17d27be727160d9df6b5d72aa264655
junjun.huang <hjjadmin@163.com>
2023-04-03 16:49:42 +0800

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,10 @@ try {
 }
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "ChatGPT by junjun",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "ChatGPT by junjun",
     statusBarStyle: "black-translucent",
   },
   themeColor: "#fafafa",
